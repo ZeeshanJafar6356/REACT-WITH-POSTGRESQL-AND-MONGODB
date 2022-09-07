46d556d2e25ed9b54f5a6aa6ea483028b8876ada
Syed Zeeshan Jafar <48754567+ZeeshanJafar6356@users.noreply.github.com>
2022-09-07 15:12:05 +0500

    Update orders.js

--- my-app/schemas/orders.js
+++ my-app/schemas/orders.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 
 let orders = new mongoose.Schema({
   // _id: mongoose.Schema.Types.ObjectId,
-  customer_id: { type: Number, required: true },
-  product_id: { type: Number, required: true },
+
+  customer_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'customer',
+  },
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'product',
+  },
   totalprice: { type: Number, required: true, unique: true },
   review: { type: Number, required: true },
 });
-module.exports = mongoose.model('order', orders);
+// module.exports = mongoose.model('order', orders);
+const Order = mongoose.model('order', orders);
+// module.exports = mongoose.model('product', products);
+module.exports = Order;
