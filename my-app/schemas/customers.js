251d59e27257d717dcd750c4af3b99bb07271aab
Syed Zeeshan Jafar <48754567+ZeeshanJafar6356@users.noreply.github.com>
2022-09-07 15:13:05 +0500

    Update customers.js

--- my-app/schemas/customers.js
+++ my-app/schemas/customers.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 let customers = new mongoose.Schema({
-  // _id: mongoose.Schema.Types.ObjectId,
   Name: { type: String, required: true },
   Phone: { type: String, required: true },
   Email: { type: String, required: true, unique: true },
   Address: { type: String, required: true },
 });
-module.exports = mongoose.model('customer', customers);
+// module.exports = mongoose.model('customer', customers);
+const Customer = mongoose.model('customer', customers);
+
+module.exports = Customer;
